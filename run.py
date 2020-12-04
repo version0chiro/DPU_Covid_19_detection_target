ca4ab1bb5b494b5dcc9fc4dcffc63116f912f6bc
Chiro <sachinsinghroxx@gmail.com>
2020-12-04 18:59:47 +0530

    checking order

--- run.py
+++ run.py
@@ -122,7 +122,10 @@ def runApp(batchSize, threads, image_dir,model):
     img = []
     print(listImage)
     for i in range(len(listImage)):
+        
         image = cv2.imread(os.path.join(image_dir,listImage[i]))
+        cv2.imshow('test',image)
+        cv2.waitKey()
         # image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
         # image = cv2.resize(image,200,200)
         image = image.reshape(-1,200,200,3).astype('float32')
